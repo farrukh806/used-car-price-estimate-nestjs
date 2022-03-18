81607a5a22f0fe039b0df98ff12fc8f5653c64e9
Farrukh <farrukhjamal806@gmail.com>
2022-03-18 20:43:30 +0530

    User association with reports added

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,14 +1,20 @@
-import { ReportsService } from './reports.service';
 import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { ReportDto } from './dtos/report.dto';
+import { Serialize } from './../interceptors/serialize.interceptor';
+import { ReportsService } from './reports.service';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { AuthGuard } from '../guards/auth.guard';
+import { CurrentUser } from '../users/decorators/current-user.decorator';
+import { User } from '../users/users.entity';
 
 @Controller('reports')
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
+
   @Post()
   @UseGuards(AuthGuard)
-  createReport(@Body() reportBody: CreateReportDto) {
-    return this.reportsService.create(reportBody);
+  @Serialize(ReportDto)
+  createReport(@Body() reportBody: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.create(reportBody, user);
   }
 }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,16 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { Report } from './reports.entity';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { User } from '../users/users.entity';
 
 @Injectable()
 export class ReportsService {
   constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
 
-  async create(reportDto: CreateReportDto) {
+  async create(reportDto: CreateReportDto, user: User) {
     try {
       const report = this.repo.create(reportDto);
+      report.user = user;
       return await this.repo.save(report);
     } catch (error) {
       throw new Error('something went wrong');
